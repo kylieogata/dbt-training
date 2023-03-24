2f40b6b1c7593361cd0414c8b7ce132303a3b91e
kogata@techstyle.com <kogata@techstyle.com>
2023-03-24 20:23:25 +0000

    developed raw_orders, raw_customer and raw_product

---
+++ models/RAW/raw_orders.sql
@@ -0,0 +1,7 @@
+
+{{
+    config(
+        materialized='table'
+    )
+}}
+select * from WORK.DBT_TRAINING_KKO.ORDERS
---
+++ models/RAW/raw_product.sql
@@ -0,0 +1,6 @@
+{{
+    config(
+        materialized='table'
+    )
+}}
+select * from WORK.DBT_TRAINING_KKO.PRODUCT
